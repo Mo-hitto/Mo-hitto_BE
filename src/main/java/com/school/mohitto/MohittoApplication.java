8b714d64e609ef1e9f7c0cf1b7ac1f182ec32dcf
alswjdghks <jerrymin1221@naver.com>
2025-04-08 02:11:13 +0900

    init: project init

---
+++ src/main/java/com/school/mohitto/MohittoApplication.java
@@ -0,0 +1,13 @@
+package com.school.mohitto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohittoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohittoApplication.class, args);
+	}
+
+}
