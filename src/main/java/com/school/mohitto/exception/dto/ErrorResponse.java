7d9746efda357b2d1134258de6f5db44215d82f9
alswjdghks <jerrymin1221@naver.com>
2025-04-08 21:52:08 +0900

    fix: ErrorResponse에 isSuccess 필드 추가

--- src/main/java/com/school/mohitto/exception/dto/ErrorResponse.java
+++ src/main/java/com/school/mohitto/exception/dto/ErrorResponse.java
@@ -5,12 +5,13 @@ import org.springframework.http.HttpStatus;
 import java.time.LocalDateTime;
 
 public record ErrorResponse(
+        boolean isSuccess,
         int status,
         String message,
         LocalDateTime timestamp
 ) {
 
     public static ErrorResponse of(HttpStatus status, String message) {
-        return new ErrorResponse(status.value(), message, LocalDateTime.now());
+        return new ErrorResponse(false,status.value(), message, LocalDateTime.now());
     }
 }
