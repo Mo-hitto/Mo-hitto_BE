3eb05f591e21d1d74c9124cc6bd2e080500e4c6e
jerrymin1221 <jerrymin1221@naver.com>
2025-04-13 19:23:09 +0900

    [docs] Swagger 설정 (#3)
    * docs: Swagger 의존성 추가 및 SwaggerConfig 설정
    
    * feat: Swagger HealthCheck API 개발

---
+++ src/main/java/com/school/mohitto/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.school.mohitto.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Health Check", description = "애플리케이션 상태 확인 API")
+@RestController
+public class HealthController {
+
+    @Operation(summary = "애플리케이션 헬스 체크", description = "서버 상태를 확인하고 정상 동작 여부를 확인하는 API")
+    @GetMapping("/health_check")
+    public String healthCheck() {
+        return "good";
+    }
+}
