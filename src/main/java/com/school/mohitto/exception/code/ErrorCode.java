5d9abdd9bf5555a3e9f1d7c152a3e6e31aab2f06
jerrymin1221 <jerrymin1221@naver.com>
2025-05-04 18:46:18 +0900

    [docs] CI 파이프라인 구축 및 Docker-compose 적용 (#10)
    * chore: .gitignore에 환경변수 파일(.env) 추가
    
    * chore: application.yml 환경 분리 및 DB 설정
    
    * chore: Dockerfile 생성
    
    * chore: Docker-compose 파일 생성(ECR에서 이미지 불러오기)
    
    * chore: CI 스크립트 작성
    
    * fix: CI 스크립트 오타 수정 및 PR 올린후 develop 브랜치로 merge 시 스크립트 실행
    
    * fix: CI 스크립트 secret 변수명 오타 수정
    
    * fix: docker-compose volume 설정 해제
    
    * [docs] 성공 응답 공통 처리 어드바이스 생성 (#6)
    
    * feat : 공통 성공 응답 구현
    
    * feat : 성공 응답 공통 처리 어드바이스 생성
    
    * [docs] 커스텀 예외 및 에러 코드/응답 구현 (#7)
    
    * feat: 커스텀 예외 및 에러 코드/응답 구현
    
    * docs: Global Exception Handler 설정
    
    * fix: ErrorResponse에 isSuccess 필드 추가

---
+++ src/main/java/com/school/mohitto/exception/code/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.school.mohitto.exception.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    BAD_REQUEST(HttpStatus.BAD_REQUEST,  "잘못된 요청입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED,  "인증이 필요합니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN,  "금지된 요청입니다."),
+    INVALID_REQUEST_INFO(HttpStatus.BAD_REQUEST,  "요청된 정보가 올바르지 않습니다."),
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "유효성 검증에 실패했습니다."),
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST,  "유효하지 않은 파라미터입니다."),
+    METHOD_ARGUMENT_TYPE_MISMATCH(HttpStatus.BAD_REQUEST,"Enum Type이 일치하지 않아 Binding에 실패하였습니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED,"지원하지 않는 HTTP method 입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,  "서버 에러, 관리자에게 문의 바랍니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+}
