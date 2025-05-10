d34cc1cf22bcb749d16db56bcec98ff1674873cc
alswjdghks <jerrymin1221@naver.com>
2025-05-10 19:15:58 +0900

    feat: 설정 파일에서 카카오 OAuth2 설정값을 가져오는 기능 추가

--- src/main/java/com/school/mohitto/config/PropertiesConfiguration.java
+++ src/main/java/com/school/mohitto/config/PropertiesConfiguration.java
@@ -1,11 +1,15 @@
 package com.school.mohitto.config;
 
 import com.school.mohitto.security.jwt.JwtProperties;
+import com.school.mohitto.security.oauth2.KakaoOAuth2Properties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(JwtProperties.class)
+@EnableConfigurationProperties({
+        JwtProperties.class,
+        KakaoOAuth2Properties.class
+})
 public class PropertiesConfiguration{
 
 }
