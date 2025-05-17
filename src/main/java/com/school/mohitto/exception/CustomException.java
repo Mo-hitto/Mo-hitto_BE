1afad8b9437c660599a04fb1707fca78f289ad5d
jerrymin1221 <jerrymin1221@naver.com>
2025-05-17 18:03:28 +0900

    [Bug] Swagger 버전 충돌 에러 수정 및 CustomException 메세지 에러 수정 (#25)
    * fix: swagger 버전 충돌 에러 수정
    
    * fix:CustumException 메세지 오류 수정

--- src/main/java/com/school/mohitto/exception/CustomException.java
+++ src/main/java/com/school/mohitto/exception/CustomException.java
@@ -1,11 +1,19 @@
 package com.school.mohitto.exception;
 
 import com.school.mohitto.exception.code.ErrorCode;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class CustomException extends RuntimeException {
     private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage()); // ✅ 메시지를 부모 클래스에 전달해야 함
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
 }
+
