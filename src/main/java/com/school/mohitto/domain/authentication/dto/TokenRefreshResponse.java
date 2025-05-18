542dac38c525e13df61182c7d648002f90ef790f
alswjdghks <jerrymin1221@naver.com>
2025-05-18 20:02:45 +0900

    feat: 인증/인가 관련 Dto 추가

--- src/main/java/com/school/mohitto/domain/authentication/dto/TokenRefreshResponse.java
+++ src/main/java/com/school/mohitto/domain/authentication/dto/TokenRefreshResponse.java
@@ -2,12 +2,11 @@ package com.school.mohitto.domain.authentication.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record LoginResult (
-        @Schema(description = "사용자 id", example = "1")
-        Long id,
+public record TokenRefreshResponse(
         @Schema(description = "서비스 Access Token", example = "Bearer access-token-abcdefghijklmn")
         String accessToken,
-        @Schema(description = "서비스 Refresh Token", example = "Bearer access-token-abcdefghijklmn")
+
+        @Schema(description = "서비스 Refresh Token", example = "Bearer refresh-token-opqrstuvwxyz")
         String refreshToken
-){
+) {
 }
