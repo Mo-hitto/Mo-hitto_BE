95aaf59c21f95100d75dea06238421dd1acf0458
alswjdghks <jerrymin1221@naver.com>
2025-05-26 21:23:11 +0900

    fix: CORS URL 여러 개일때 고려해 배포 설정 변경

--- src/main/java/com/school/mohitto/security/CorsProperties.java
+++ src/main/java/com/school/mohitto/security/CorsProperties.java
@@ -3,7 +3,8 @@ package com.school.mohitto.security;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "spring.cors")
-public record CorsProperties(
-        String[] allowedOrigins
-){
+public record CorsProperties(String allowedOrigins) {
+    public String[] getAllowedOrigins() {
+        return allowedOrigins != null ? allowedOrigins.split(",") : new String[0];
+    }
 }
