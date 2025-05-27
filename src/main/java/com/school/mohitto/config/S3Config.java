e1a5fdd57a3d3fc9c04bb0502629ed852dc60d6b
alswjdghks <jerrymin1221@naver.com>
2025-05-27 16:08:40 +0900

    feat: Amazon S3 클라이언트 설정을 위한 S3Config 추가

---
+++ src/main/java/com/school/mohitto/config/S3Config.java
@@ -0,0 +1,29 @@
+package com.school.mohitto.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.school.mohitto.aws.s3.S3Properties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class S3Config {
+
+    private final S3Properties s3Properties;
+
+    @Bean
+    public AmazonS3 s3Client() {
+        BasicAWSCredentials credentials =
+                new BasicAWSCredentials(s3Properties.accessKey(), s3Properties.secretKey());
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(s3Properties.region())
+                .build();
+    }
+}
