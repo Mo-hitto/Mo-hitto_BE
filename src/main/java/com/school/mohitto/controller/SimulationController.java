1205386731c6e73a93124309f84d7a19f3adfccb
alswjdghks <jerrymin1221@naver.com>
2025-05-30 03:43:10 +0900

    feat: AI 모델을 이용해 사용자의 얼굴사진과 설문조사결과를 바탕으로 미용실/헤어 추천해주는 서비스 API 개발

--- src/main/java/com/school/mohitto/controller/SimulationController.java
+++ src/main/java/com/school/mohitto/controller/SimulationController.java
@@ -1,9 +1,14 @@
 package com.school.mohitto.controller;
 
+import com.school.mohitto.dto.requestDTO.SimulationRequest;
+import com.school.mohitto.dto.responseDTO.RecommandResponse;
+import com.school.mohitto.service.SimulationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/simulation")
@@ -11,4 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Simulation", description = "AI 모델 관련 API")
 public class SimulationController {
 
+    private final SimulationService simulationService;
+
+    @PostMapping("/recommand")
+    public RecommandResponse getRecommand(
+            @RequestPart(value = "image", required = false)
+            MultipartFile multipartFile,
+            @RequestBody SimulationRequest simulationRequest
+    ) throws IOException {
+        return simulationService.extractFaceAndRecommand(multipartFile,simulationRequest);
+    }
 }
