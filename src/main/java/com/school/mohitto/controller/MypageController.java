863f4ea9217534fd07c644e43a95c9b63447d887
alswjdghks <jerrymin1221@naver.com>
2025-05-30 19:09:09 +0900

    fix: 마이페이지 관련 API 컨트롤러 이름 변경 및 사용자 정보 가져오는 API 추가

--- src/main/java/com/school/mohitto/controller/MypageController.java
+++ src/main/java/com/school/mohitto/controller/MypageController.java
@@ -2,9 +2,11 @@ package com.school.mohitto.controller;
 
 import com.school.mohitto.domain.authentication.AuthUserInfo;
 import com.school.mohitto.dto.responseDTO.HairResponseList;
+import com.school.mohitto.dto.responseDTO.MypageUserInfoResponse;
 import com.school.mohitto.exception.annotation.PageConstraint;
 import com.school.mohitto.security.annotation.AuthUser;
 import com.school.mohitto.service.HairService;
+import com.school.mohitto.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -21,14 +23,15 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/hairs")
-@Tag(name = "Hair", description = "헤어스타일 저장/조회 API")
-public class HairController {
+@RequestMapping("/mypage")
+@Tag(name = "Mypage", description = "마이페이지 API")
+public class MypageController {
 
     private final HairService hairService;
+    private final UserService userService;
 
     @Operation(summary = "저장한 헤어스타일 목록 조회")
-    @GetMapping
+    @GetMapping("hair/saved")
     public HairResponseList getSavedHairs(
             @AuthUser AuthUserInfo authUserInfo,
             @ParameterObject
@@ -48,15 +51,30 @@ public class HairController {
             @ApiResponse(responseCode = "404", description = "해당 헤어스타일을 찾을 수 없음"),
             @ApiResponse(responseCode = "500", description = "서버 오류")
     })
-    @DeleteMapping("/{hairId}")
+    @DeleteMapping("hair/delete/{hairId}")
     public ResponseEntity<String> deleteSavedHair(
             @AuthUser AuthUserInfo authUserInfo,
-
             @Parameter(description = "삭제할 헤어스타일 ID", required = true)
             @PathVariable Long hairId
     ) {
         hairService.deleteSavedHair(authUserInfo.userId(), hairId);
         return ResponseEntity.ok("삭제되었습니다.");
+    }
+
+    @Operation(
+        summary = "마이페이지 유저 정보 조회",
+        description = "마이페이지 상단에 보여줄 유저의 닉네임, 프로필이미지 등을 조회합니다."
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "조회 성공"),
+        @ApiResponse(responseCode = "401", description = "인증 실패"),
+        @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @GetMapping("/userInfo")
+    public MypageUserInfoResponse getMypageUserInfo(
+            @AuthUser AuthUserInfo authUserInfo
+    ){
+        return userService.getMypageUserInfo(authUserInfo.userId());
     }
 }
 
