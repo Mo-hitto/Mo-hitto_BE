57da96a085353a544b9f620de555208270aee3fa
alswjdghks <jerrymin1221@naver.com>
2025-05-31 22:55:38 +0900

    feat: ModelImageRepository 개발

---
+++ src/main/java/com/school/mohitto/repository/ModelImageRepository.java
@@ -0,0 +1,7 @@
+package com.school.mohitto.repository;
+
+import com.school.mohitto.domain.ModelImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModelImageRepository extends JpaRepository<ModelImage, Long> {
+}
