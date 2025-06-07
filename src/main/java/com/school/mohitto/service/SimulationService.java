f1c4a03b142466491b59bec5611119267c09ff59
alswjdghks <jerrymin1221@naver.com>
2025-06-07 17:38:17 +0900

    fix: 모델이미지 확인을 위한 로그정보 추가

--- src/main/java/com/school/mohitto/service/SimulationService.java
+++ src/main/java/com/school/mohitto/service/SimulationService.java
@@ -199,6 +199,7 @@ public class SimulationService {
                 recommendation ->
                 {
                     ModelImage modelImage = modelImageRepository.findModelImageByDiagnosisFeature(diagnosis,recommendation.style());
+                    log.info(modelImage.toString());
                     Hair hair = Hair.builder()
                             .name(recommendation.style())
                             .explanation(recommendation.description())
