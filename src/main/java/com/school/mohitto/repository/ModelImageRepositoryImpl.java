5f694a841c6391a35656cf431dfe9765f4564b84
alswjdghks <jerrymin1221@naver.com>
2025-06-07 18:23:24 +0900

    fix: 여자는 hairType 을 None으로 처리해도 모델이미지 가져오도록 수정

--- src/main/java/com/school/mohitto/repository/ModelImageRepositoryImpl.java
+++ src/main/java/com/school/mohitto/repository/ModelImageRepositoryImpl.java
@@ -1,8 +1,13 @@
 package com.school.mohitto.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.school.mohitto.domain.Diagnosis;
 import com.school.mohitto.domain.ModelImage;
+import com.school.mohitto.domain.enums.HairTypeEnum;
+import com.school.mohitto.domain.enums.SexType;
 import lombok.RequiredArgsConstructor;
 
 import static com.school.mohitto.domain.QModelImage.*;
@@ -14,13 +19,22 @@ public class ModelImageRepositoryImpl implements ModelImageRepositoryCustom {
 
     @Override
     public ModelImage findModelImageByDiagnosisFeature(Diagnosis diagnosis, String style) {
+
+        BooleanBuilder hairTypeCondition = new BooleanBuilder();
+
+        if (diagnosis.getDiagnosisSex().getSex().getSex() == SexType.MALE) {
+            hairTypeCondition.and(modelImage.hairTypeEnum.eq(diagnosis.getDiagnosisHairType().getHairType().getType()));
+        } else {
+            hairTypeCondition.and(modelImage.hairTypeEnum.eq(HairTypeEnum.NONE));
+        }
+
         ModelImage image = queryFactory.selectFrom(modelImage)
                 .where(
                         modelImage.name.eq(style),
                         modelImage.hairLength.eq(diagnosis.getDiagnosisHairLength().getHairLength().getHairLength()),
                         modelImage.sex.eq(diagnosis.getDiagnosisSex().getSex().getSex()),
-                        modelImage.hairTypeEnum.eq(diagnosis.getDiagnosisHairType().getHairType().getType()),
-                        modelImage.hasBangType.eq(diagnosis.getDiagnosisHasbangs().getHasBangs().getHasBangType())
+                        modelImage.hasBangType.eq(diagnosis.getDiagnosisHasbangs().getHasBangs().getHasBangType()),
+                        hairTypeCondition
                 )
                 .fetchFirst();
         return image;
